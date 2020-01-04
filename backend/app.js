2ab0245bdc695d02d49fad7b61e595eb1302a56c
Tim <autumn5816@gmail.com>
2020-01-04 18:15:37 -0500

    first version of blog platform

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postRoutes = require("./routes/posts")
+
+const app = express();
+
+// 6sX5z6RUWVdDS1s7
+
+mongoose.connect("mongodb+srv://Tim:6sX5z6RUWVdDS1s7@cluster0-jpf9k.mongodb.net/node-angular?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postRoutes);
+
+module.exports = app;
